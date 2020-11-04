cb5ca3e985f8656868870cb1b204b5236c4a8158
ildar <tt@tt.com>
2020-11-04 15:49:34 +0300

    fix sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,33 +1,28 @@
-DELETE FROM User;
-DELETE FROM Document;
-DELETE FROM Country;
-DELETE FROM Doc;
-DELETE FROM Office;
-DELETE FROM Organization;
+INSERT INTO Doc (version, code, name) VALUES (0, 21, 'Паспорт гражданина Российской Федерации');
+INSERT INTO Doc (version, code, name) VALUES (0, 12, 'Вид на жительство в Российской Федерации');
 
-INSERT INTO Doc (code, name) VALUES (21, 'Паспорт гражданина Российской Федерации');
-INSERT INTO Doc (code, name) VALUES (12, 'Вид на жительство в Российской Федерации');
+INSERT INTO Country (version, code, name) VALUES (0, 643, 'Российская Федерация');
+INSERT INTO Country (version, code, name) VALUES (0, 578, 'Королевство Норвегия');
 
-INSERT INTO Country (code, name) VALUES (643, 'Российская Федерация');
-INSERT INTO Country (code, name) VALUES (578, 'Королевство Норвегия');
+INSERT INTO Organization (version, name, full_name, inn, kpp, address, phone, is_active)
+    VALUES (0, 'Альфа', 'ООО Альфа', 9977001234, 987654321, 'ул.Мира, 24', 84951112233, true);
+INSERT INTO Organization (version, name, full_name, inn, kpp, address, phone, is_active)
+    VALUES (0, 'Бета', 'ООО Бета', 9977004444, 987651111, 'ул.Кирова, 1', 84952223344, true);
 
-INSERT INTO Organization (name, full_name, inn, kpp, address, phone, is_active)
-    VALUES ('Вектор', 'ООО Вектор', 9977001234, 987654321, 'ул.Мира, 24', 84951112233, true);
+INSERT INTO Office (version, name, address, phone, is_active, org_id)
+    VALUES (0, 'Офис1 Альфа', 'ул.Мира, 24', 84951112233, true, 2);
+INSERT INTO Office (version, name, address, phone, is_active, org_id)
+    VALUES (0, 'Офис2 Альфа', 'ул.Правды, 3', 84958889900, true, 2);
 
-INSERT INTO Office (name, address, phone, is_active, org_id)
-    VALUES ('БЦ Океан', 'ул.Мира, 24', 84951112233, true, 1);
-INSERT INTO Office (name, address, phone, is_active, org_id)
-    VALUES ('БЦ Полянка', 'ул.Правды, 3', 84958889900, true, 1);
+INSERT INTO Document (version, doc_id, doc_number, doc_date)
+VALUES (0, 2, 4365364, '2025-12-31');
+INSERT INTO Document (version, doc_id, doc_number, doc_date)
+VALUES (0, 2, 7554432, '2026-11-30');
 
-INSERT INTO Document (doc_type, doc_number, doc_date, citizenship_code, user_id)
-VALUES (12, 4365364, '2025-12-31', 578, 1);
-INSERT INTO Document (doc_type, doc_number, doc_date, citizenship_code, user_id)
-VALUES (12, 7554432, '2026-11-30', 578, 2);
-
-INSERT INTO User (first_name, second_name, middle_name, position, phone, document_id, is_identified, office_id)
-    VALUES ('John', 'Doe', null, 'manager', 89990001122, 1, true, 2);
-INSERT INTO User (first_name, second_name, middle_name, position, phone, document_id, is_identified, office_id)
-    VALUES ('Mary', 'Public', null, 'hr', 89990002222, 2, true, 1);
+INSERT INTO User (version, first_name, second_name, position, phone, document_id, citizenship_id, is_identified, office_id)
+    VALUES (0, 'John', 'Doe', 'manager', 89990001122, 1, 2, true, 2);
+INSERT INTO User (version, first_name, second_name, position, phone, document_id, citizenship_id, is_identified, office_id)
+    VALUES (0, 'Mary', 'Public', 'hr', 89990002222, 2, 2, true, 1);
 
 
 
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,70 +5,78 @@ DROP TABLE IF EXISTS Doc;
 DROP TABLE IF EXISTS Office;
 DROP TABLE IF EXISTS Organization;
 
+CREATE TABLE Doc (
+    id         INTEGER                     COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+    version    INTEGER            NOT NULL COMMENT 'Служебное поле hibernate',
+    code       INTEGER UNIQUE     NOT NULL COMMENT 'Код',
+    name       VARCHAR(50) UNIQUE NOT NULL COMMENT 'Документ'
+);
+COMMENT ON TABLE Doc IS 'Справочник Код документа';
+
+CREATE TABLE Country (
+    id         INTEGER                     COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+    version    INTEGER            NOT NULL COMMENT 'Служебное поле hibernate',
+    code       INTEGER UNIQUE     NOT NULL COMMENT 'Код',
+    name       VARCHAR(50) UNIQUE NOT NULL COMMENT 'Страна'
+);
+COMMENT ON TABLE Country IS 'Справочник Гражданство';
+
+CREATE TABLE Document (
+    id               INTEGER                     COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+    version          INTEGER            NOT NULL COMMENT 'Служебное поле hibernate',
+    doc_id           INTEGER            NOT NULL COMMENT 'Идентификатор справочника документа',
+    FOREIGN KEY (doc_id) REFERENCES Doc (id),
+    doc_number       VARCHAR(20) UNIQUE NOT NULL COMMENT 'Номер документа',
+    doc_date         DATE               NOT NULL COMMENT 'Дата документа'
+);
+COMMENT ON TABLE Document IS 'Документ';
+CREATE INDEX IX_document_docid ON Document (doc_id);
+
 CREATE TABLE Organization (
-    id         INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-    name       VARCHAR(20) NOT NULL COMMENT 'Имя',
-    full_name  VARCHAR(50) NOT NULL COMMENT 'Полное имя',
-    inn        VARCHAR(10) NOT NULL COMMENT 'ИНН',
-    kpp        VARCHAR(9)  NOT NULL COMMENT 'КПП',
-    address    VARCHAR(50) NOT NULL COMMENT 'Адрес',
-    phone      VARCHAR(11) NOT NULL COMMENT 'Телефон',
-    is_active  BOOLEAN     NOT NULL COMMENT 'Активен'
+    id         INTEGER                     COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+    version    INTEGER            NOT NULL COMMENT 'Служебное поле hibernate',
+    name       VARCHAR(20) UNIQUE NOT NULL COMMENT 'Имя',
+    full_name  VARCHAR(50)        NOT NULL COMMENT 'Полное имя',
+    inn        VARCHAR(10)        NOT NULL COMMENT 'ИНН',
+    kpp        VARCHAR(9)         NOT NULL COMMENT 'КПП',
+    address    VARCHAR(50)        NOT NULL COMMENT 'Адрес',
+    phone      VARCHAR(11)                 COMMENT 'Телефон',
+    is_active  BOOLEAN            NOT NULL COMMENT 'Активность'
 );
 COMMENT ON TABLE Organization IS 'Организация';
-CREATE INDEX IX_Organization_Name ON Organization (name);
+CREATE INDEX IX_organization_name ON Organization (name);
 
 CREATE TABLE Office (
-    id         INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-    name       VARCHAR(20) NOT NULL COMMENT 'Имя',
+    id         INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+    version    INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
+    name       VARCHAR(50) NOT NULL COMMENT 'Имя',
     address    VARCHAR(50) NOT NULL COMMENT 'Адрес',
     phone      VARCHAR(11) NOT NULL COMMENT 'Телефон',
-    is_active  BOOLEAN     NOT NULL COMMENT 'Активен',
+    is_active  BOOLEAN     NOT NULL COMMENT 'Активность',
     org_id     INTEGER              COMMENT 'Идентификатор организации',
     FOREIGN KEY (org_id) REFERENCES Organization (id) ON DELETE CASCADE
 );
 COMMENT ON TABLE Organization IS 'Офис';
-CREATE INDEX IX_Office_ID_OrgId ON Office (id, org_id);
-
-CREATE TABLE Country (
-    code  INTEGER     NOT NULL COMMENT 'Код страны' PRIMARY KEY,
-    name  VARCHAR(50) NOT NULL COMMENT 'Страна'
-);
-COMMENT ON TABLE Country IS 'Страна';
-
-CREATE TABLE Doc (
-    code       INTEGER     NOT NULL COMMENT 'Код' PRIMARY KEY,
-    name       VARCHAR(50) NOT NULL COMMENT 'Документ'
-);
-COMMENT ON TABLE Doc IS 'Код документа';
-
-CREATE TABLE Document (
-    id               INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-    doc_type         INTEGER     NOT NULL COMMENT 'Код документа',
-    FOREIGN KEY (doc_type) REFERENCES Doc (code),
-    doc_number       VARCHAR(20) NOT NULL COMMENT 'Номер документа',
-    doc_date         DATE        NOT NULL COMMENT 'Дата документа',
-    citizenship_code INTEGER     NOT NULL COMMENT 'Код гражданства',
-    FOREIGN KEY (citizenship_code) REFERENCES Country (code),
-    user_id          INTEGER              COMMENT 'Идентификатор организации',
---     FOREIGN KEY (user_id) REFERENCES User (id) ON DELETE CASCADE,
-    CONSTRAINT UX_Document_ID_UserId UNIQUE (id, user_id)
-);
-COMMENT ON TABLE Document IS 'Документ';
+CREATE INDEX IX_office_orgid ON Office (org_id);
 
 CREATE TABLE User (
-    id               INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-    first_name       VARCHAR(20) NOT NULL COMMENT 'Имя',
-    second_name      VARCHAR(20) NOT NULL COMMENT 'Фамилия',
-    middle_name      VARCHAR(20)          COMMENT 'Отчество',
+    id               INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+    version          INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
+    first_name       VARCHAR(30) NOT NULL COMMENT 'Имя',
+    second_name      VARCHAR(30)          COMMENT 'Фамилия',
+    middle_name      VARCHAR(30)          COMMENT 'Отчество',
     position         VARCHAR(20) NOT NULL COMMENT 'Должность',
-    phone            VARCHAR(11) NOT NULL COMMENT 'Телефон',
+    phone            VARCHAR(11)          COMMENT 'Телефон',
     document_id      INTEGER     NOT NULL COMMENT 'Идентификатор документа',
     FOREIGN KEY (document_id) REFERENCES Document (id),
+    citizenship_id   INTEGER     NOT NULL COMMENT 'Идентификатор справочника гражданства',
+    FOREIGN KEY (citizenship_id) REFERENCES Country (id),
     is_identified    BOOLEAN     NOT NULL COMMENT 'Идентифицирован',
     office_id        INTEGER     NOT NULL COMMENT 'Идентификатор офиса',
     FOREIGN KEY (office_id) REFERENCES Office (id) ON DELETE CASCADE
 );
 COMMENT ON TABLE User IS 'Сотрудник';
-CREATE INDEX IX_User_Id_OfficeId ON User (id, office_id);
+CREATE UNIQUE INDEX UX_user_documentid ON User (document_id);
+CREATE INDEX IX_user_documentid ON User (citizenship_id);
+CREATE INDEX IX_user_officeid ON User (office_id);
 
